b21893006a59088addd772dba7c2bfa7632d85ec
Duhsten <dustinkosborne@gmail.com>
2017-05-16 00:51:01 -0400

    Basic script

---
+++ Assets/scripts/playermovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playermovement : MonoBehaviour
+{
+    Animator playercontroller;
+    int jumpHash = Animator.StringToHash("Jump");
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        playercontroller = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+      
+            float move = Input.GetAxis("Vertical");
+            playercontroller.SetFloat("Speed", move);
+
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                playercontroller.SetTrigger(jumpHash);
+            }
+        else
+        {
+            playercontroller.ResetTrigger("Jump");
+        }
+
+        
+    }
+}
